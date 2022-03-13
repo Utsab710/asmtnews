8462fec33914eaef0c291dbb708ef116fb09b63e
Utsab Shrestha <utsabsshrestha710@gmail.com>
2022-03-13 11:19:35 +0545

    update

---
+++ db/edit-category.php
@@ -0,0 +1,16 @@
+<?php 
+if(!isset($_POST['category'])){
+    die("can not edit the record");
+}else{
+    $c = $_POST['category'];
+    $i = $_POST['iconClass'];
+    $id = $_POST['id'];
+    include('connect.php');
+    $query ="UPDATE category SET title='$c',iconImage='$i' WHERE id='$id'";
+    if(mysqli_query($conn,$query)){
+        header('location:../category.php?msg=successfully update');
+    }else{
+        header('location:../category.php?errmsg='.mysqli_error($conn));
+    }
+}
+?>
--- edit-category.php
+++ edit-category.php
@@ -38,7 +38,8 @@
              <div class="row">
             <?php include('include/left-nav.php');?>    
                <div class="col-8">
-                <form method="POST" action="db/add-category.php">
+                <form method="POST" action="db/edit-category.php">
+                    <input type="hidden" name="id" value="<?php echo $cid;?>">
                     <label>Category title</label>
                     <div class="input-group">
                         <input type="text" value="<?php echo $row['title'];?>" class="form-control" name="category">
