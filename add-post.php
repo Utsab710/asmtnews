2c02655b1b0c98a4ee9540e735934399f3792de2
Utsab Shrestha <utsabsshrestha710@gmail.com>
2022-03-15 14:40:26 +0545

    hi

--- add-post.php
+++ add-post.php
@@ -1,70 +1,66 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['login']) || !$_SESSION['login']==1){
-    header('Location:login.php');
-    }
-    $id = $_SESSION['user_id'];
-    include('db/connect.php');
-    $query = "SELECT * FROM users WHERE id='$id'";
-    $result = mysqli_query($conn,$query);
-    $data = mysqli_fetch_assoc($result);
+session_start();
+ if(!isset($_SESSION['login']) || !$_SESSION['login']==1){
+   header('Location:login.php');
+ }
+ $id = $_SESSION['user_id']; 
+ include('db/connect.php');
+ $query = "SELECT * FROM users WHERE id='$id'";
+$result = mysqli_query($conn,$query);
+$data = mysqli_fetch_assoc($result);
 
-    $categoryQuery ="SELECT * FROM category";
-    $categoryResult=mysqli_query($conn,$categoryQuery);
-
+$categoryQuery = "SELECT *FROM category";
+$categoryResult = mysqli_query($conn,$categoryQuery);
 
 ?>
 <!DOCTYPE html>
 <html>
-        <head>
-            <title>Home-Asmt News</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        </head>
-      <body>
-            <?php include('include/nav.php');?>    
-           
-            <div class="container">
-             <div class="row">
-            <?php include('include/left-nav.php');?>  
+  <head>
+      <title>Home-Asmt News</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  </head>
+<body>
+<?php include('include/nav.php');?>    
+ 
+<div class="container">
+  <div class="row">
+  <?php include('include/left-nav.php');?>
+      <div class="col-8">
+        <form method="POST"action="db/add-post.php" enctype="multipart/form-data">
+         <div class="mb-3">
+           <label for="" class="form-label">Title</label>
+           <input type="text" class="form-control" name="title">
+         </div>
 
-               <div class="col-8">
-                <form method="POST" action="#">
+         <div class="mb-3">
+           <label for="" class="form-label"></label>
+           <textarea id="news" class="form-control" name="content"></textarea>
+         </div>
 
-                  <div class="mb-3">
-                    <label for="" class="form-label">Title</label>
-                    <input type="text" class="form-control" name="title">
-                  </div>
+         <div class="mb-3">
+           <label for="" class="form-label">Cover Image</label>
+            <input type="file" name="image">
+         </div>
+          
+         <div class="mb-3">
+           <label for="" class="form-label">Category</label>
+           <select class="form-control" name="category">
+             <?php while($row=mysqli_fetch_assoc($categoryResult)){ ?>
+              <option value="<?php echo $row['id'];?>">
+              <?php echo $row['title']; ?></option>
+             <?php } ?>
+           </select>
+         </div>
+         <hr/>
+         <button type="submit" class="btn btn-success">Save</button>
+        </form>
+      </div>
+    </div>
+  </div>
 
-                  <div class="mb-3">
-                    <label for="" class="form-label"></label>
-                   <textarea id="news" class="form-control" name="content"></textarea>
-                  </div>
-
-                  <div class="mb-3">
-                    <label for="" class="form-label">Cover Image</label>
-                  <input type="file" name="image">
-                  </div>
-
-                  <div class="mb-3">
-                    <label for="" class="form-label">Category</label>
-                      <select class="form-control" name="category">
-                        <?php while ($row=mysqli_fetch_assoc($categoryResult)){ ?>
-                              <option value="<?php echo $row['id']; ?>">
-                              <?php echo $row['title']; ?> </option>
-                        <?php } ?>
-                      </select>
-                  </div>
-                  <hr/>
-                  <button type="submit" class="btn btn-success">Save</button>
-
-                </form>
-               </div>
-             </div>
-            </div>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-      
-      <script src="https://cdn.tiny.cloud/1/ngkfkvpfz6ch1jzpuqtjcu37z27gctu4fjnebl8yz6gom41j/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-      <script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.tiny.cloud/1/chbsy7ou5qr0st3ayjfs672rs9b5cx9wv9s8tse49k0kbo42/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
     
 var useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
@@ -129,7 +125,7 @@ tinymce.init({
  });
 
 
-
   </script>
-    </body>
+</script>
+</body>
 </html>
